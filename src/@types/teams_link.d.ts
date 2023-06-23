78b57481e68fdc821c71cf8b64f09117436794f6
xpa <xpadev@gmail.com>
2023-06-23 23:26:27 +0900

    feat: POST /teams/:teamId/links

--- src/@types/teams_link.d.ts
+++ src/@types/teams_link.d.ts
@@ -3,3 +3,10 @@ import { ILink } from "@/@types/entity/ILink";
 export type ITeamsLinkResponse = {
   links: ILink[];
 };
+
+export type IPostTeamsLinkBody = {
+  link: {
+    left: string;
+    right: string;
+  };
+};
--- src/endpoints/teams_link.ts
+++ src/endpoints/teams_link.ts
@@ -2,10 +2,11 @@ import { RouteHandlerMethodWrapper } from "@/endpoints/RouteHandlerMethodWrapper
 import { ITeamParams } from "@/@types";
 import { getUser } from "@/utils/auth";
 import { source } from "@/database";
-import { Link, Team } from "@/entity";
+import { Link, Team, Comment } from "@/entity";
 import { isUserInTeamAndThrow } from "@/utils/isUserInTeamAndThrow";
-import { ITeamsLinkResponse } from "@/@types/teams_link";
+import { IPostTeamsLinkBody, ITeamsLinkResponse } from "@/@types/teams_link";
 import { FastifyInstance } from "fastify";
+import { uuid } from "@/utils/uuid";
 
 const get_teams_teamId_links: RouteHandlerMethodWrapper<{
   Params: ITeamParams;
@@ -22,8 +23,35 @@ const get_teams_teamId_links: RouteHandlerMethodWrapper<{
   });
 };
 
+const post_teams_teamId_links: RouteHandlerMethodWrapper<{
+  Params: ITeamParams;
+  Body: IPostTeamsLinkBody;
+  Reply: ITeamsLinkResponse;
+}> = async (request, reply) => {
+  const { left, right } = request.body.link;
+  const teamId = request.params.teamId;
+  const user = await getUser(request);
+  const team = await source.manager.findOne(Team, { where: { id: teamId } });
+  if (!team) throw new Error("The team is not found.");
+  isUserInTeamAndThrow(user, team);
+  const leftComment = await source.manager.findOneBy(Comment, { id: left });
+  const rightComment = await source.manager.findOneBy(Comment, { id: right });
+  if (!leftComment || !rightComment) throw new Error("comment not found");
+  const link = new Link();
+  link.id = uuid();
+  link.left = leftComment;
+  link.right = rightComment;
+  link.team = team;
+  await source.manager.save(link);
+  const links = await source.manager.find(Link, { where: { team } });
+  await reply.status(200).send({
+    links,
+  });
+};
+
 const setupTeamsLinks = (app: FastifyInstance) => {
   app.get("/teams/:teamId/links", get_teams_teamId_links);
+  app.post("/teams/:teamId/links", post_teams_teamId_links);
 };
 
 export { setupTeamsLinks };
