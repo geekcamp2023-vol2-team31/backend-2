5b4230eefd689cad7c5ead8f5f6217880e0a8f5c
xpa <xpadev@gmail.com>
2023-06-23 20:58:48 +0900

    feat: DELETE /teams/:teamId/comments/:commentId

--- src/endpoints/teams_comments.ts
+++ src/endpoints/teams_comments.ts
@@ -91,12 +91,35 @@ const put_teams_teamId_comments_commentId: RouteHandlerMethodWrapper<{
   });
 };
 
+const delete_teams_teamId_comments_commentId: RouteHandlerMethodWrapper<{
+  Params: ITeamsCommentParams;
+  Reply: ITeamsCommentResponse;
+}> = async (request, reply) => {
+  const { teamId, commentId } = request.params;
+  const user = await getUser(request);
+  const team = await source.manager.findOne(Team, { where: { id: teamId } });
+  if (!team) throw new Error("The team is not found.");
+  isUserInTeamAndThrow(user, team);
+  const comment = await source.manager.findOne(Comment, {
+    where: { team, id: commentId },
+  });
+  await source.manager.remove(comment);
+  const comments = await source.manager.find(Comment, { where: { team } });
+  await reply.status(200).send({
+    comments,
+  });
+};
+
 const setupTeamsComments = (app: FastifyInstance) => {
   app.get("/teams/:teamId/comments", get_teams_teamId_comments);
   app.post("/teams/:teamId/comments", post_teams_teamId_comments);
   app.put(
     "/teams/:teamId/comments/:commentId",
     put_teams_teamId_comments_commentId
+  );
+  app.delete(
+    "/teams/:teamId/comments/:commentId",
+    delete_teams_teamId_comments_commentId
   );
 };
 
